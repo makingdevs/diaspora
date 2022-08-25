920a01ad23dfa37bd3a274ee3291b335b61dbd81
Jose Juan Reyes <neodevelop@gmail.com>
2022-08-25 13:58:31 -0500

    Get common followers of followers

--- src/main/groovy/com/makingdevs/diaspora/Network.groovy
+++ src/main/groovy/com/makingdevs/diaspora/Network.groovy
@@ -6,4 +6,18 @@ class Network {
   List commonFollowers(followersForUser1, followersForUser2) {
     followersForUser1 - (followersForUser1 - followersForUser2)
   }
+
+  def gridOfUsersAndFollowers(gridOfUsers) {
+    def allUsers = gridOfUsers.collect { e -> e.collect { k, v  -> k} }.flatten()
+
+    allUsers.collect { user ->
+      gridOfUsers.collect { userAndFollowers ->
+        userAndFollowers.find { thisUser, followers ->
+          followers.any { follower -> follower == user}
+        }
+      }.findAll { it }.collect { entries ->
+        entries.collect { entry -> entry.key }
+      }.flatten()
+    }
+  }
 }
