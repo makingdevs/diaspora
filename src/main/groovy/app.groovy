62091cc3e26cc7f077cbac7c3abdf73af45df2c6
Jose Juan Reyes <neodevelop@gmail.com>
2022-08-29 16:55:22 -0500

    Adjust app for User class

--- src/main/groovy/app.groovy
+++ src/main/groovy/app.groovy
@@ -6,17 +6,11 @@ def network = Network.instance
 
 user_1 = client.getUser("neodevelop")
 followers_1 = client.getFollowersForUser(user_1)
-userWithFollowers_1 = [:]
-userWithFollowers_1."${user_1.login}" = followers_1.flatten()*.login
-println userWithFollowers_1
-println "-"*100
+user_1.followers = followers_1
 
 user_2 = client.getUser("carlogilmar")
 followers_2 = client.getFollowersForUser(user_2)
-userWithFollowers_2 = [:]
-userWithFollowers_2."${user_2.login}" = followers_2.flatten()*.login
-println userWithFollowers_2
-println "-"*100
+user_2.followers = followers_2
 
-commonFollowers = network.commonFollowers(userWithFollowers_1.first(), userWithFollowers_2.first())
-println commonFollowers
+commonFollowers = network.commonFollowersFor(user_1, user_2)
+println commonFollowers*.username
