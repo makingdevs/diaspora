ca9c0fe3a2de284aaedb1354da5b3e0a28638164
Jose Juan Reyes <neodevelop@gmail.com>
2022-08-29 22:29:44 -0500

    Get the followers for the users and the relation between them

--- src/main/groovy/com/makingdevs/diaspora/Network.groovy
+++ src/main/groovy/com/makingdevs/diaspora/Network.groovy
@@ -9,36 +9,30 @@ class Network {
     followersFor1 - (followersFor1 - followersFor2)
   }
 
-  def gridOfUsersAndFollowers(gridOfUsers) {
-    def allUsers = gridOfUsers.collect { e -> e.collect { k, v  -> k} }.flatten()
+  def gridOfUsersAndFollowers(List<User> users) {
 
-    allUsers.inject([:]) { acc, user ->
-      def followers = gridOfUsers.collect { userAndFollowers ->
-        userAndFollowers.find { thisUser, followers ->
-          followers.any { follower -> follower == user}
-        }
-      }.findAll { it }.collect { entries ->
-        entries*.key
-      }.flatten()
-
-      acc.put(user, followers)
-      acc
+    def grid = [:]
+    users.each { currentUser ->
+      List followers = []
+      users.each { anotherUser ->
+        if(anotherUser.followers.any { u -> u.username == currentUser.username })
+          followers << anotherUser
+      }
+      grid.put(currentUser.username, followers*.username)
     }
+    grid
   }
 
-  def graphvizForFollowers(user1, user2) {
-    def username1 = user1.keySet().flatten().first()
-    def username2 = user2.keySet().flatten().first()
-    def commonFollowersFor = commonFollowersFor(user1, user2)
-    commonFollowersFor.collect { follower ->
-        "${username1} -> ${follower};\n${username2} -> ${follower};"
+  def graphvizForFollowers(user1, user2, commonFollowers) {
+    commonFollowers.collect { follower ->
+        "\"${user1.username}\" -> \"${follower.username}\";\n\"${user2.username}\" -> \"${follower.username}\";"
     }.join("\n")
   }
 
-  def graphvizForFollowersOfFollowers(Map gridOfUsers) {
-    gridOfUsers.collect { user, followers ->
-      followers.collect { follower ->
-        "${user} -> ${follower};"
+  def graphvizForFollowersOfFollowers(users) {
+    users.collect { user ->
+      user.value.collect { follower ->
+        "\"${user.key}\" -> \"${follower}\";"
       }.join("\n")
     }.join("\n")
   }
