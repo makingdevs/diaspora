553867299872d3a4a93f2e667c2bd5daea7ab828
Jose Juan Reyes <neodevelop@gmail.com>
2022-08-29 22:28:02 -0500

    Do the output for the graph

--- src/main/groovy/app.groovy
+++ src/main/groovy/app.groovy
@@ -11,4 +11,19 @@ user_2 = client.getUser("carlogilmar")
 user_2 = client.putFollowersToUser(user_2)
 
 commonFollowers = network.commonFollowersFor(user_1, user_2)
-println commonFollowers*.username
+
+commonFollowers = commonFollowers.collect { user ->
+  user = client.getUser(user.username)
+  user = client.putFollowersToUser(user)
+  user
+}
+
+grid = network.gridOfUsersAndFollowers(commonFollowers)
+
+graphCommonFollowers = network.graphvizForFollowers(user_1, user_2, commonFollowers)
+graph = network.graphvizForFollowersOfFollowers(grid)
+//println graph
+file = new File("graph.dot")
+file.text = graphCommonFollowers + graph
+
+println "Done!!!"
